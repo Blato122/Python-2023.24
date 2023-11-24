6dea8a03fc76ea27d1dc3dab1c87675cbb5f14fb
blato <blator02@gmail.com>
2023-11-24 16:00:20 +0100

    paddle bounce, refactoring

--- zestaw9/pong.py
+++ zestaw9/pong.py
@@ -21,13 +21,59 @@ pygame.display.set_caption('Pong')
 
 # ----------------------- VARIABLES -----------------------
 paddle_width = screen_width / 70
-paddle_height = screen_height / 7
+paddle_height = screen_height / 5
 paddle_speed = 15
 
-ball_diameter = paddle_height / 5
+ball_diameter = screen_width / 50
 paddle_offset = ball_diameter * 1.5
 
 ball_init_speed = [8, 8]
+
+# ----------------------- GAME -----------------------
+class Pong:
+    def __init__(self, ball, paddle_l, paddle_r=None):
+        self.paddle_l = paddle_l
+        self.paddle_r = paddle_r
+        self.ball = ball
+
+        self.paddles = pygame.sprite.Group()
+        self.paddles.add(paddle_l) # player 1
+        self.paddles.add(paddle_r if paddle_r is not None else paddle_l.copy()) # player 2 / AI
+
+        self.sprites = pygame.sprite.Group(self.ball, self.paddles)
+
+        self.score_l = self.score_r = 0
+        self.MAX_SCORE = 11
+        self.over = False
+        self.point_scored = False
+
+    def run(self):
+        # HANDLE EVENTS
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.over = True
+
+        # DRAWING
+        if pygame.sprite.spritecollideany(self.ball, self.paddles):
+            ball.change_direction()
+
+        keys = pygame.key.get_pressed()
+        self.sprites.update(keys)
+
+
+        screen.fill(BLACK)
+        self.sprites.draw(screen)
+
+        clock.tick(2*fps)
+        pygame.display.flip() # przerysowanie całego okna z bufora na ekran
+
+    def update_score(self, score):
+        score += 1
+        if score == self.MAX_SCORE:
+            self.end_game()
+
+    def end_game(self):
+        pass
 
 # ----------------------- PADDLE -----------------------
 class Paddle(pygame.sprite.Sprite):
@@ -59,58 +105,52 @@ class Ball(pygame.sprite.Sprite):
         self.image = pygame.Surface([diameter, diameter])
         self.image.fill(color) # tylko kolor
         self.rect = self.image.get_rect()
-        self.rect.y = (screen_height - ball_diameter) / 2
-        self.rect.x = (screen_width - ball_diameter) / 2
-        
+        self.init_coords()
+        self.is_scored = False
+        self.speed = self.rand_spd()
+
+    def rand_spd(self):
         # so as to avoid movement too vertical or too horizontal
-        angle_beg = random.choice([20, 110, 200, 290])
-        angle_end = angle_beg + 50
+        angle_beg = random.choice([30, 120, 210, 300])
+        angle_end = angle_beg + 30
         angle = math.radians(random.uniform(angle_beg, angle_end))
         x_speed = ball_init_speed[0] * math.sin(angle)
         y_speed = ball_init_speed[1] * math.cos(angle)
-        self.speed = [x_speed, y_speed]
+        return [x_speed, y_speed]
+    
+    def init_coords(self):
+        self.rect.y = (screen_height - ball_diameter) / 2
+        self.rect.x = (screen_width - ball_diameter) / 2
 
-    def update(self):
+    def change_direction(self):
+        self.speed[0] *= -1
+
+    def update(self, keys):
         self.rect.move_ip(self.speed)
-        if self.rect.left < 0 or self.rect.right > screen_width:
-            self.speed[0] *= -1
-        if self.rect.top < 0 or self.rect.bottom > screen_height:
+
+        if self.rect.top <= 0 or self.rect.bottom >= screen_height:
             self.speed[1] *= -1
+        if self.rect.right < paddle_offset or self.rect.left > screen_width - paddle_offset:
+            self.speed = [0, 0]
+            self.is_scored = True
+        
+        if self.is_scored:
+            if keys[pygame.K_SPACE]:
+                self.is_scored = False
+                self.init_coords()
+                self.speed = self.rand_spd()
+            
+        return self.is_scored
 
 # ----------------------- SPRITES -----------------------
 ball = Ball(RED, ball_diameter)
-balls = pygame.sprite.Group()
-balls.add(ball)
+paddle1 = Paddle(GREEN, paddle_width, paddle_height, left=True)
+paddle2 = Paddle(GREEN, paddle_width, paddle_height)
 
-paddle1 = Paddle(GREEN, paddle_width, paddle_height)
-paddle2 = Paddle(GREEN, paddle_width, paddle_height, left=True)
-paddles = pygame.sprite.Group()
-paddles.add(paddle1, paddle2)
-
-# ----------------------- CLOCK -----------------------
-fps = 60
-clock = pygame.time.Clock()
-
-# ----------------------- MAIN GAME LOOP -----------------------
-score = 0
-done = False
-while not done:
-    clock.tick(fps)
-    screen.fill(BLACK)
-
-    # HANDLE EVENTS
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    # DRAWING
-    # Rysowanie różnych obiektów, sprawdzanie przekrywania, itp.
-    keys = pygame.key.get_pressed()
-    paddles.update(keys)
-    paddles.draw(screen)
-    balls.update()
-    balls.draw(screen)
-    pygame.display.flip() # przerysowanie całego okna z bufora na ekran
-
-pygame.quit()
-# dalsze instrukcje programu bez pygame
+# ----------------------- GAME -----------------------
+if __name__ == "__main__":
+    fps = 60
+    clock = pygame.time.Clock()
+    game = Pong(ball, paddle1, paddle2)
+    game.run()
+    pygame.quit()
